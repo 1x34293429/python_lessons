0a70ef40a054e4911ec3eaec7d80518853d2695b
iskandar_norboyev <i.norboyev309@gmail.com>
2023-07-04 15:36:31 +0500

    1-python
    1-fayl

---
+++ python-1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  4 15:30:25 2023
+
+@author: User
+"""
+
+yosh = int(input("Yoshingiz nechada: "))
+if yosh<=4:
+    print("Sizga kirish bepul")
+elif yosh<=12:
+    print("Sizga kirish 5000 so'm ")
+else:
+    print("Sizga kirish 10000 so'm")
